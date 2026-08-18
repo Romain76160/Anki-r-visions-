f5f9db4a292844b59e09558634dca4211965f713
Romain76160 <149166435+Romain76160@users.noreply.github.com>
2026-08-18 17:38:47 +0200

    Update service worker for v3 Safari bundle

--- sw.js
+++ sw.js
@@ -1,27 +1,11 @@
-const CACHE='mooc-revision-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./starter_questions.json','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-
+const CACHE='mooc-revision-v3';
+const ASSETS=['./','./index.html','./styles.css?v=3','./app-v2.js?v=3','./manifest.webmanifest?v=3','./starter_questions.json?v=2','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
-      if(response && response.ok){
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      }
-      return response;
-    }).catch(()=>caches.match('./index.html')))
-  );
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
 });
